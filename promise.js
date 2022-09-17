289dde023bb3247cd17bd5e756f73dfbd781d661
fukamila <farahulfahkamila17@gmail.com>
2022-09-17 14:28:52 +0800

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,8 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi) => {
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+
+    let jumlahEmosi = 0;
+    theaterIXX.forEach(hitung);
+    theaterVGC.forEach(hitung);
+
+    function hitung(item, index) {
+        if (item.hasil == emosi) { jumlahEmosi++ }
+    }
+    return jumlahEmosi;
+};
 
 module.exports = {
-  promiseOutput,
+    promiseOutput,
 };
